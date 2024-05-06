86af97e789aaa0f4006019bed2582b9a914f77b3
Firdavs Nazarov <fredrickuzb55@gmail.com>
2024-05-06 16:47:21 +0900

    Initial commit

---
+++ car.js
@@ -0,0 +1,9 @@
+
+
+const car = {
+    name: "Mers"
+};
+
+// console.log(car.name);
+
+module.exports = car; // Corrected module.exports statement
---
+++ index.js
@@ -0,0 +1,11 @@
+
+// console.log("Hello world");
+
+// const example= () => console.log("Hello Firdavs")
+
+// example()
+
+const carDate = require('./car.js')
+
+console.log(carDate.name);
+
